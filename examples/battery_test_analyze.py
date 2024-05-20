16ce43aabea65a0267a278ae7bc6b7d7aef5d1c5
Jaakko Korhonen <13328062+korjaa@users.noreply.github.com>
2024-05-20 15:32:20 +0300

    Parse battery manufacturer and load current from the log file.

--- examples/battery_test_analyze.py
+++ examples/battery_test_analyze.py
@@ -1,3 +1,4 @@
+import re
 import logging
 import pathlib
 from typing import List
@@ -11,6 +12,13 @@ logger = logging.getLogger(__name__)
 
 
 def process_file(csv_file: pathlib.Path, color: str, axs: List[plt.Axes]):
+    match = re.match(
+        r"(?P<make>\w+)_(?P<ah>\d+)ah_(?P<t_relax>\d+)s_(?P<t_load>\d+)s_(?P<A>\d+)A_.*",
+        csv_file.name)
+    make = match.group("make")
+    capacity = float(match.group("ah"))
+    load_current = int(match.group("A"))
+    logging.info(f"Parsed {make} battery {capacity:.1f} Ah with {load_current} A load current.")
     df = pandas.read_csv(
         csv_file,
         delimiter=",",
@@ -18,19 +26,19 @@ def process_file(csv_file: pathlib.Path, color: str, axs: List[plt.Axes]):
         index_col=0
     )
     df["Cell Voltage"] = df["Voltage"] / 5
-    df["Loaded"] = df["Current"] > 9.9
+    df["Loaded"] = df["Current"] > (0.99 * load_current)
     df["Relaxed"] = df["Current"] < 0.1
 
     # Remove sloping current (Load transition cleaning 1)
     smaller_than = df["Current"] < 0.1
-    larger_than = df["Current"] > 9.9
+    larger_than = df["Current"] > (0.99 * load_current)
     valid_values = smaller_than | larger_than
     df = df[valid_values]
     logger.info(f"Removed {sum(~valid_values)} transition samples")
 
     # Erode close by samples (Load transition cleaning 2)
     mask = scipy.ndimage.binary_erosion(
-        input=df["Current"].diff().abs() < 9,
+        input=df["Current"].diff().abs() < (0.9 * load_current),
         iterations=2
     )
     df = df[mask]
@@ -48,12 +56,11 @@ def process_file(csv_file: pathlib.Path, color: str, axs: List[plt.Axes]):
 
     # Calculate Voltage drop and resistance
     df["Battery Voltage Drop"] = df["Relaxed Battery Voltage"] - df["Loaded Battery Voltage"]
-    df["Battery Resistance mOhm"] = (df["Battery Voltage Drop"] / 10.0) * 1e3
+    df["Battery Resistance mOhm"] = (df["Battery Voltage Drop"] / load_current) * 1e3
 
     df.plot(ax=axs[0], x="AH", y=["Relaxed Battery Voltage", "Loaded Battery Voltage"],
             color=color, legend=False)
-    df.plot(ax=axs[1], x="AH", y="Battery Resistance mOhm",
-            color=color, legend=False)
+    df.plot(ax=axs[1], x="AH", y="Battery Resistance mOhm", color=color, legend=False)
 
 
 def process_files(csv_files: List[pathlib.Path], plt_name: pathlib.Path):
@@ -78,7 +85,7 @@ def process_files(csv_files: List[pathlib.Path], plt_name: pathlib.Path):
 
     # Post adjust
     for ax in axs:
-        ax.set_xlim([0, 4])
+        #ax.set_xlim([0, 5])
         ax.grid(True)
         if ax != axs[-1]:
             ax.tick_params(axis="x", which="both", labelbottom=False)
@@ -87,14 +94,30 @@ def process_files(csv_files: List[pathlib.Path], plt_name: pathlib.Path):
     fig.set_size_inches(10, 10)
     plt.subplots_adjust(left=0.07, bottom=0.1, right=0.95, top=0.90, hspace=0.1)
     plt.savefig(plt_name)
+    #plt.show()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    process_files(
-        csv_files=list(pathlib.Path(".").glob("ryobi_[45]ah_*old.txt")),
-        plt_name=pathlib.Path("compare_old_batteries.png"))
+    #process_files(
+    #    csv_files=list(pathlib.Path(".").glob("ryobi_[45]ah_*old.txt")),
+    #    plt_name=pathlib.Path("compare_old_batteries.png"))
+
+    #process_files(
+    #    csv_files=list(pathlib.Path(".").glob("ryobi_[45]ah_*new.txt")),
+    #    plt_name=pathlib.Path("compare_new_batteries.png"))
+
+    #process_files(
+    #    csv_files=list(pathlib.Path("battery_test_example_results").glob(
+    #        "ryobi_*_15A_*269180*.txt")),
+    #    plt_name=pathlib.Path("compare_269180.png"))
+
+    #process_files(
+    #    csv_files=list(pathlib.Path("battery_test_example_results").glob(
+    #        "ryobi_*269184*.txt")),
+    #    plt_name=pathlib.Path("compare_269184.png"))
 
     process_files(
-        csv_files=list(pathlib.Path(".").glob("ryobi_[45]ah_*new.txt")),
-        plt_name=pathlib.Path("compare_new_batteries.png"))
+        csv_files=list(pathlib.Path("battery_test_example_results").glob(
+            "ninebot_*.txt")),
+        plt_name=pathlib.Path("ninebot.png"))
